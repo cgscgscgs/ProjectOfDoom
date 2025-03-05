f2b645d7113139cdf880b9546bb70453f90582be
Celina G Schlecht <156964413+cgscgscgs@users.noreply.github.com>
2025-03-05 12:48:04 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*  date            developer              changes
+ *  3/5/2025        cgscgs                Initial Creation  
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOfDoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Howdy");
+
+        }
+    }
+}
